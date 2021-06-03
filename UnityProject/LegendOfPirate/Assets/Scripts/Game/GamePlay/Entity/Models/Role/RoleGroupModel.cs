5ee42307fc72f0aaffbe0da5349e34149c1d2dee
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-06-03 10:13:54 +0800

    RoleModel

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/Models/Role/RoleGroupModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace GameWish.Game
+{
+    [ModelAutoRegister]
+	public class RoleGroupModel : DbModel
+	{
+        public ReactiveCollection<RoleModel> roleItemList = new ReactiveCollection<RoleModel>();
+
+        protected override void LoadDataFromDb()
+        {
+            RoleGroupData roleGroupData = GameDataMgr.S.GetRoleGroupData();
+            for (int i = 0; i < roleGroupData.roleList.Count; i++)
+            {
+                RoleData roleData = roleGroupData.roleList[i];
+                RoleModel itemModel = new RoleModel(roleData);
+                roleItemList.Add(itemModel);
+            }
+        }
+    }    
+}
---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/Models/Role/RoleModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+    public class RoleModel : Model
+    {
+        public int id;
+        public IntReactiveProperty level;
+        public string name;
+        public string resName;
+        public ReactiveCollection<RoleEquipModel> equipList;
+        public ReactiveCollection<RoleSkillModel> skillList;
+
+        public RoleModel(RoleData roleData)
+        {
+            id = roleData.id;
+            level = new IntReactiveProperty(roleData.level);
+            name = roleData.name;
+
+            equipList = new ReactiveCollection<RoleEquipModel>();
+            for (int i = 0; i < roleData.equipList.Count; i++)
+            {
+                RoleEquipModel equipModel = new RoleEquipModel(roleData.equipList[i]);
+                equipList.Add(equipModel);
+            }
+
+            skillList = new ReactiveCollection<RoleSkillModel>();
+            for (int i = 0; i < roleData.skillList.Count; i++)
+            {
+                RoleSkillModel skillModel = new RoleSkillModel(roleData.skillList[i]);
+                skillList.Add(skillModel);
+            }
+        }
+
+        public bool AddSkill()
+        {
+            return false;
+        }
+
+        public void UpgradeSkill()
+        {
+
+        }
+
+        public bool AddEquip()
+        {
+            return false;
+        }
+
+        public void UpgradeEquip()
+        {
+
+        }
+    }
+
+}
