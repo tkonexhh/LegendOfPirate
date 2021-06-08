153dd724e9eb102d0da3222727451f13dd994805
ZYJ <ginn.zhang@modooplay.com>
2021-06-08 10:10:21 +0800

    PanelUpdate

--- UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/SkillLevelUpPanel/SkillLevelUpPanel.Binder.cs
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/SkillLevelUpPanel/SkillLevelUpPanel.Binder.cs
@@ -8,32 +8,52 @@ namespace GameWish.Game
 {
 	public class SkillLevelUpPanelData : UIPanelData
 	{
+		public RoleSkillModel roleSkillModel { get; private set; }
+
 		public SkillLevelUpPanelData()
 		{
+			roleSkillModel = ModelMgr.S.GetModel<RoleModel>().GetSkillModel(1);
 		}
+		public void SetRoleSkillModel(int skillid)
+		{
+			roleSkillModel = ModelMgr.S.GetModel<RoleModel>().GetSkillModel(skillid);
+		}
+		
 	}
 	
 	public partial class SkillLevelUpPanel
 	{
 		private SkillLevelUpPanelData m_PanelData = null;
-		
+		public int skillId { get; set; }
 		private void AllocatePanelData()
 		{
 			 m_PanelData = UIPanelData.Allocate<SkillLevelUpPanelData>();
+			 m_PanelData.SetRoleSkillModel(skillId);
 		}
 		
 		private void ReleasePanelData()
 		{
 			ObjectPool<SkillLevelUpPanelData>.S.Recycle(m_PanelData);
 		}
-		
-		private void BindModelToUI()
+        private void InitPanelWithParams(params object[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                skillId = (int)args[0];
+            }
+        }
+        private void BindModelToUI()
 		{
+			m_PanelData.roleSkillModel.skillLevel.Subscribe(level => SkillLevel.text = "Lv." + level) ;
+		
+		}
+
+       
+        private void BindUIToModel()
+		{
+			CloseBtn.OnClickAsObservable().Subscribe(_ => HideSelfWithAnim());
 		}
 		
-		private void BindUIToModel()
-		{
-		}
 		
 	}
 }
