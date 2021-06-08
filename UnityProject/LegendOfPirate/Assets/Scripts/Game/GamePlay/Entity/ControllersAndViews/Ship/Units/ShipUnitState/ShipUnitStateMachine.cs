363bc65c81d8320933d1d37606358750446bb287
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-06-08 10:59:44 +0800

    ShipState

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/ControllersAndViews/Ship/Units/ShipUnitState/ShipUnitStateMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public class ShipUnitStateMachine : FSMStateMachine<ShipUnit>
+    {
+        public ShipUnitStateMachine(ShipUnit entity) : base(entity)
+        {
+            stateFactory = new FSMStateFactory<ShipUnit>(false);
+
+            //stateFactory.RegisterState(BattleStateEnum.Fighting, new BattleState_Fighting());
+            //stateFactory.RegisterState(BattleStateEnum.End, new BattleState_End());
+        }
+    }
+
+}
