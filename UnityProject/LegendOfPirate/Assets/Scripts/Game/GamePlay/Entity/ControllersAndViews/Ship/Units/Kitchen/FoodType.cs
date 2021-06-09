7b6e5481666a7d284dfd6da4a8c974ec728c0547
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-06-09 19:41:34 +0800

    ShipUnit

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/ControllersAndViews/Ship/Units/Kitchen/FoodType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+    //和表中的FoodId对应
+	public enum FoodType
+	{
+        None,
+        Food1,
+	}
+	
+}
---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/ControllersAndViews/Ship/Units/ShipUnitBaseProperty.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+	public struct ShipUnitBaseProperty
+	{
+        public int level;
+        public UpgradeCost upgradeCost;
+    }
+
+    public struct UpgradeCost
+    {
+
+    }
+}
