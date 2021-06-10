44a47b5362c4d61015b8c6d9e504e0103cd9fa45
Lvmm <mengmeng.lv@modooplay.com>
2021-06-10 14:02:14 +0800

    更新

--- UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/TrainingRoomPanel/TrainingRoomPanel.Binder.cs
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/TrainingRoomPanel/TrainingRoomPanel.Binder.cs
@@ -7,66 +7,79 @@ using System;
 
 namespace GameWish.Game
 {
-	public class TrainingRoomPanelData : UIPanelData
-	{
-		public TrainingRoomPanelData()
-		{
+    public class TrainingRoomPanelData : UIPanelData
+    {
+        public ShipModel shipModel;
+        public TrainingRoomModel trainingRoomModel;
+        public TrainingRoomPanelData()
+        {
 
-		}
-	}
-	
-	public partial class TrainingRoomPanel
-	{
+        }
+    }
 
-		private TrainingRoomPanelData m_PanelData = null;
+    public partial class TrainingRoomPanel
+    {
+
+        private TrainingRoomPanelData m_PanelData = null;
 
 
-		private string iconName;
-		private StringReactiveProperty reactiveProperty1 = new StringReactiveProperty("6");
+        private void AllocatePanelData(params object[] args)
+        {
+            m_PanelData = UIPanelData.Allocate<TrainingRoomPanelData>();
 
-		private void AllocatePanelData(params object[] args)
-		{
-			 m_PanelData = UIPanelData.Allocate<TrainingRoomPanelData>();
+            try
+            {
+                m_PanelData.shipModel = ModelMgr.S.GetModel<ShipModel>();
 
-		}
+                m_PanelData.trainingRoomModel = m_PanelData.shipModel.GetShipUnitModel(ShipUnitType.TrainingRoom) as TrainingRoomModel;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("e = " + e);
+            }
+        }
 
         private void ReleasePanelData()
-		{
-			ObjectPool<TrainingRoomPanelData>.S.Recycle(m_PanelData);
-		}
-		
-		private void BindModelToUI()
-		{
-            reactiveProperty1
-				.Select(str => CommonMethod.GetStringForTableKey(LanguageKeyDefine.Fixed_Title_Lv) + str)
-				.SubscribeToTextMeshPro(TrainingLevelTMP);
+        {
+            ObjectPool<TrainingRoomPanelData>.S.Recycle(m_PanelData);
         }
-		
-		private void BindUIToModel()
-		{
-		}
 
-		private void RegisterEvents()
-		{
+        private void BindModelToUI()
+        {
+            m_PanelData.trainingRoomModel
+                .level
+                .Select(level => CommonMethod.GetStringForTableKey(LanguageKeyDefine.Fixed_Title_Lv) + level.ToString())
+                .SubscribeToTextMeshPro(TrainingLevelTMP);
+        }
 
-		}
+        private void BindUIToModel()
+        {
+        }
 
-		private void OnClickAddListener()
-		{
-			TrainingUpgradeBtn.OnClickAsObservable().Subscribe(_ => {
-				Debug.LogError("Upgrade");//TODO
-			});
-			TrainBtn.OnClickAsObservable().Subscribe(_ => {
-				Debug.LogError("Train");//TODO
-			});
-			AutoTrainBtn.OnClickAsObservable().Subscribe(_ => {
-				Debug.LogError("AutoTrain");//TODO
-			});
-		}
-		private void UnregisterEvents()
-		{
+        private void RegisterEvents()
+        {
 
-		}
+        }
 
-	}
+        private void OnClickAddListener()
+        {
+            TrainingUpgradeBtn.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.LogError("Upgrade");//TODO
+            });
+            TrainBtn.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.LogError("Train");//TODO
+            });
+            AutoTrainBtn.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.LogError("AutoTrain");//TODO
+            });
+        }
+        private void UnregisterEvents()
+        {
+
+        }
+
+    }
 }
