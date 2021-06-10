b9dca07e59794e4fb920338ab7a158c4a7f2945a
Lvmm <mengmeng.lv@modooplay.com>
2021-06-10 11:13:58 +0800

    黑市部分UI

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/AlchemyRoomPanel/BottomAlchemyPotion.cs
@@ -0,0 +1,17 @@
+using Qarth;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+    public class BottomAlchemyPotion : UListItemView
+    {
+        internal void OnInit(int index)
+        {
+        }
+    }
+
+}
