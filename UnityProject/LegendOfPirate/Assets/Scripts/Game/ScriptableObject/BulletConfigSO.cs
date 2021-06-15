dd817939873603d6346261b30b356b9af2a9d3e7
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-06-15 10:36:51 +0800

    Update

--- UnityProject/LegendOfPirate/Assets/Scripts/Game/ScriptableObject/BulletConfigSO.cs
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/ScriptableObject/BulletConfigSO.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,4 +24,29 @@ namespace GameWish.Game
         public BulletTargetType TargetType;
     }
 
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace GameWish.Game
+{
+    [CreateAssetMenu(menuName = "Game/BulletConfigSO", fileName = "new_BulletConfigSO")]
+    public class BulletConfigSO : SerializedScriptableObject
+    {
+        [LabelText("子弹ID")]
+        public int ID;
+        [LabelText("子弹速度")]
+        public float Speed = 0.5f;
+        [LabelText("子弹预制体")]
+        public GameObject Prefab;
+        [LabelText("爆炸特效")]
+        public GameObject Effect;
+
+        public BulletMoveType MoveType;
+        public BulletTargetType TargetType;
+    }
+
+>>>>>>> origin/Zyj
 }
