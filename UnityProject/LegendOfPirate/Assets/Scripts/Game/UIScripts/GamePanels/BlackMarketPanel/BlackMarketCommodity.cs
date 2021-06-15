334d6d0332119617405c662e6170840be6465c7d
Lvmm <mengmeng.lv@modooplay.com>
2021-06-15 17:13:18 +0800

    选角色功能

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/BlackMarketPanel/BlackMarketCommodity.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Qarth;
+
+
+namespace GameWish.Game
+{
+	public class BlackMarketCommodity : UListItemView
+	{
+		#region  Ù–‘
+		[SerializeField]
+		private TextMeshProUGUI m_Name;	
+		[SerializeField]
+		private Image m_Icom;	
+		
+		[SerializeField,Header("State1")]
+		private GameObject m_State1;
+		[SerializeField, Header("State2")]
+		private GameObject m_State2;
+		#endregion
+
+		#region ∑Ω∑®
+		public void OnInit(BlackMarketCommodityModule blackMarketCommodityModule)
+		{
+			
+		}
+		#endregion
+
+		#region Region3
+		#endregion
+
+		#region Region4
+		#endregion
+	}
+
+}
