e6acd7794968c523a541a5c693cb2f4b2746d746
chuck hu <chuck.hu@modooplay.com>
2021-06-17 20:47:45 +0800

    fix mainsea level

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/MainSeaLevelPanel/MainSeaLevelPanel.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Qarth.Extension;
+using Qarth;
+using UniRx;
+
+namespace GameWish.Game
+{
+	public partial class MainSeaLevelPanel : AbstractAnimPanel
+	{
+		protected override void OnUIInit()
+		{
+			base.OnUIInit();
+			
+			AllocatePanelData();
+			
+			BindModelToUI();
+			
+			BindUIToModel();
+			
+		}
+		
+		protected override void OnPanelOpen(params object[] args)
+		{
+			base.OnPanelOpen(args);
+			
+		}
+		
+		protected override void OnPanelHideComplete()
+		{
+			base.OnPanelHideComplete();
+			
+			CloseSelfPanel();
+		}
+		
+		protected override void OnClose()
+		{
+			base.OnClose();
+			
+		}
+		
+		protected override void BeforDestroy()
+		{
+			base.BeforDestroy();
+			
+			ReleasePanelData();
+		}
+		
+	}
+}
