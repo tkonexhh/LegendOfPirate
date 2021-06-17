8e8014be4edf4c68d372b18df13a929927949763
burgess.chen <burgess.chen@modooplay.com>
2021-06-17 20:34:03 +0800

    Add MarinLevelConfigTable

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/Tables/Generate/Gameplay/TDMarinLevelConfig.cs
@@ -0,0 +1,138 @@
+//Auto Generate Don't Edit it
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public partial class TDMarinLevelConfig
+    {
+        
+       
+        private EInt m_Level = 0;   
+        private EInt m_Chapter = 0;   
+        private string m_Type;   
+        private string m_ResOutput;   
+        private string m_EnemyHeadIcon;   
+        private string m_Enemies;   
+        private string m_Reward;   
+        private string m_RecommendAtkValue;   
+        private string m_BattleName;  
+        
+        //private Dictionary<string, TDUniversally.FieldData> m_DataCacheNoGenerate = new Dictionary<string, TDUniversally.FieldData>();
+      
+        /// <summary>
+        /// ID
+        /// </summary>
+        public  int  level {get { return m_Level; } }
+       
+        /// <summary>
+        /// 所属章节
+        /// </summary>
+        public  int  chapter {get { return m_Chapter; } }
+       
+        /// <summary>
+        /// 关卡类型
+        /// </summary>
+        public  string  type {get { return m_Type; } }
+       
+        /// <summary>
+        /// 产出资源
+        /// </summary>
+        public  string  resOutput {get { return m_ResOutput; } }
+       
+        /// <summary>
+        /// 敌人头像
+        /// </summary>
+        public  string  enemyHeadIcon {get { return m_EnemyHeadIcon; } }
+       
+        /// <summary>
+        /// 敌人配置，格式ID:|数量|功力;…
+        /// </summary>
+        public  string  enemies {get { return m_Enemies; } }
+       
+        /// <summary>
+        /// 奖励
+        /// </summary>
+        public  string  reward {get { return m_Reward; } }
+       
+        /// <summary>
+        /// 推荐功力
+        /// </summary>
+        public  string  recommendAtkValue {get { return m_RecommendAtkValue; } }
+       
+        /// <summary>
+        /// 战斗名称
+        /// </summary>
+        public  string  battleName {get { return m_BattleName; } }
+       
+
+        public void ReadRow(DataStreamReader dataR, int[] filedIndex)
+        {
+          //var schemeNames = dataR.GetSchemeName();
+          int col = 0;
+          while(true)
+          {
+            col = dataR.MoreFieldOnRow();
+            if (col == -1)
+            {
+              break;
+            }
+            switch (filedIndex[col])
+            { 
+            
+                case 0:
+                    m_Level = dataR.ReadInt();
+                    break;
+                case 1:
+                    m_Chapter = dataR.ReadInt();
+                    break;
+                case 2:
+                    m_Type = dataR.ReadString();
+                    break;
+                case 3:
+                    m_ResOutput = dataR.ReadString();
+                    break;
+                case 4:
+                    m_EnemyHeadIcon = dataR.ReadString();
+                    break;
+                case 5:
+                    m_Enemies = dataR.ReadString();
+                    break;
+                case 6:
+                    m_Reward = dataR.ReadString();
+                    break;
+                case 7:
+                    m_RecommendAtkValue = dataR.ReadString();
+                    break;
+                case 8:
+                    m_BattleName = dataR.ReadString();
+                    break;
+                default:
+                    //TableHelper.CacheNewField(dataR, schemeNames[col], m_DataCacheNoGenerate);
+                    break;
+            }
+          }
+
+        }
+        
+        public static Dictionary<string, int> GetFieldHeadIndex()
+        {
+          Dictionary<string, int> ret = new Dictionary<string, int>(9);
+          
+          ret.Add("Level", 0);
+          ret.Add("Chapter", 1);
+          ret.Add("Type", 2);
+          ret.Add("ResOutput", 3);
+          ret.Add("EnemyHeadIcon", 4);
+          ret.Add("Enemies", 5);
+          ret.Add("Reward", 6);
+          ret.Add("RecommendAtkValue", 7);
+          ret.Add("BattleName", 8);
+          return ret;
+        }
+    } 
+}//namespace LR
---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/Tables/Generate/Gameplay/TDMarinLevelConfigTable.cs
@@ -0,0 +1,91 @@
+﻿//Auto Generate Don't Edit it
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public static partial class TDMarinLevelConfigTable
+    {
+        private static TDTableMetaData m_MetaData = new TDTableMetaData(TDMarinLevelConfigTable.Parse, "MarinLevelConfig");
+        public static TDTableMetaData metaData
+        {
+            get { return m_MetaData; }
+        }
+        
+        private static Dictionary<int, TDMarinLevelConfig> m_DataCache = new Dictionary<int, TDMarinLevelConfig>();
+        private static List<TDMarinLevelConfig> m_DataList = new List<TDMarinLevelConfig >();
+        
+        public static void Parse(byte[] fileData)
+        {
+            m_DataCache.Clear();
+            m_DataList.Clear();
+            DataStreamReader dataR = new DataStreamReader(fileData);
+            int rowCount = dataR.GetRowCount();
+            int[] fieldIndex = dataR.GetFieldIndex(TDMarinLevelConfig.GetFieldHeadIndex());
+    #if (UNITY_STANDALONE_WIN) || UNITY_EDITOR || UNITY_STANDALONE_OSX
+            dataR.CheckFieldMatch(TDMarinLevelConfig.GetFieldHeadIndex(), "MarinLevelConfigTable");
+    #endif
+            for (int i = 0; i < rowCount; ++i)
+            {
+                TDMarinLevelConfig memberInstance = new TDMarinLevelConfig();
+                memberInstance.ReadRow(dataR, fieldIndex);
+                OnAddRow(memberInstance);
+                memberInstance.Reset();
+                CompleteRowAdd(memberInstance, rowCount);
+            }
+            Log.i(string.Format("Parse Success TDMarinLevelConfig"));
+        }
+
+        private static void OnAddRow(TDMarinLevelConfig memberInstance)
+        {
+            int key = memberInstance.level;
+            if (m_DataCache.ContainsKey(key))
+            {
+                Log.e(string.Format("Invaild,  TDMarinLevelConfigTable Id already exists {0}", key));
+            }
+            else
+            {
+                m_DataCache.Add(key, memberInstance);
+                m_DataList.Add(memberInstance);
+            }
+        }    
+        
+        public static void Reload(byte[] fileData)
+        {
+            Parse(fileData);
+        }
+
+        public static int count
+        {
+            get 
+            {
+                return m_DataCache.Count;
+            }
+        }
+
+        public static List<TDMarinLevelConfig> dataList
+        {
+            get 
+            {
+                return m_DataList;
+            }    
+        }
+
+        public static TDMarinLevelConfig GetData(int key)
+        {
+            if (m_DataCache.ContainsKey(key))
+            {
+                return m_DataCache[key];
+            }
+            else
+            {
+                Log.w(string.Format("Can't find key {0} in TDMarinLevelConfig", key));
+                return null;
+            }
+        }
+    }
+}//namespace LR
