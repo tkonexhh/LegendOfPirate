aec746dcac03454eaca527b6cf5a311a638e349c
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-06-18 10:13:54 +0800

    ShipRole状态机与代码位置调整

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/VC/Role/RoleMonoReference.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+namespace GameWish.Game
+{
+    public class RoleMonoReference : MonoBehaviour
+    {
+        public AIPath AstarAI;
+        public CapsuleCollider Collider;
+    }
+
+}
---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/GamePlay/Entity/VC/Role/ShipRole/ShipRoleState/State/ShipRoleState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public enum ShipRoleStateId
+    {
+        Locked,
+        ReadyToBuild,
+        Building,
+        Idle,
+    }
+
+    public class ShipRoleState : FSMState<ShipRoleController>
+    {
+        public ShipRoleStateId stateID
+        {
+            get;
+            set;
+        }
+
+        public ShipRoleState(ShipRoleStateId stateEnum)
+        {
+            stateID = stateEnum;
+        }
+    }
+
+}
