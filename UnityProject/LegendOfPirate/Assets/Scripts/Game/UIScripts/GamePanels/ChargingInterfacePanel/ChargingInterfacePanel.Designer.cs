192eb255ee9f0fdc85de7bd2790b90b7665f6032
Lvmm <mengmeng.lv@modooplay.com>
2021-07-01 11:13:48 +0800

    内购UI

---
+++ UnityProject/LegendOfPirate/Assets/Scripts/Game/UIScripts/GamePanels/ChargingInterfacePanel/ChargingInterfacePanel.Designer.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Qarth.Extension;
+using Qarth;
+using TMPro;
+
+namespace GameWish.Game
+{
+	// Auto Generated Code. Don't Modify This Class 
+	public partial class ChargingInterfacePanel
+	{
+		public const string Name = "ChargingInterfacePanel";
+		
+		[SerializeField] private TextMeshProUGUI m_VipDiamondsNumber;
+		[SerializeField] private TextMeshProUGUI m_VipDiamondsCountDown;
+		[SerializeField] private RectTransform m_NotPurchasedState;
+		[SerializeField] private TextMeshProUGUI m_TotalDiamondsNumber;
+		[SerializeField] private TextMeshProUGUI m_TodayDiamondsNumber;
+		[SerializeField] private TextMeshProUGUI m_EveryDiamondsNumber;
+		[SerializeField] private TextMeshProUGUI m_QuantityReceivedNumber;
+		[SerializeField] private TextMeshProUGUI m_ProfitValue;
+		[SerializeField] private TextMeshProUGUI m_VipDetailsRatio;
+		[SerializeField] private TextMeshProUGUI m_DueDate;
+		[SerializeField] private Button m_PurchaseBtn;
+		[SerializeField] private Image m_VipIcon;
+		[SerializeField] private TextMeshProUGUI m_SelectionRefreshTime;
+		[SerializeField] private RectTransform m_SelectionItemReion;
+		[SerializeField] private RectTransform m_PurchaseDiamondsRegion;
+		
+		
+	}
+}
